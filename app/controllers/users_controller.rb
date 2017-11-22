887631af45b51a45448527e658e56b55115e87f6
Casey Jordan <Jordan_Casey.bah.com>
2017-11-22 16:56:14 -0500

    added users tweets to their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.find_by user_id: @user
+    @tweets = Tweet.find_by user_id: @user.id
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,2 @@
 Tweet.destroy_all
 Reply.destroy_all
-
-Tweet.create(content: "Hello World!")
-Tweet.create(content: "This is a Tweet")
-
-Reply.create(content: "Hi.", tweet_id: 1)
-Reply.create(content: "Yes it is", tweet_id: 1)
