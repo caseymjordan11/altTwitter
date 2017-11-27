30049d79f68b897f19d8aea7e8d26f3592e29501
Casey Jordan <Jordan_Casey.bah.com>
2017-11-27 12:19:47 -0500

    added a users index file so can find user to follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @tweets = Tweet.find_by user_id: @user.id
