4cb9368494494ca0275a7dda4680750696794473
Casey Jordan <Jordan_Casey.bah.com>
2017-11-29 09:57:45 -0500

    going to work through retweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126000920) do
+ActiveRecord::Schema.define(version: 20171129145229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20171126000920) do
     t.bigint "user_id"
     t.index ["tweet_id"], name: "index_replies_on_tweet_id"
     t.index ["user_id"], name: "index_replies_on_user_id"
+  end
+
+  create_table "retweets", force: :cascade do |t|
   end
 
   create_table "tweets", force: :cascade do |t|
