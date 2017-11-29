e3f8571ff0dc708ac9879026c85eb76050339cc6
Casey Jordan <Jordan_Casey.bah.com>
2017-11-29 10:47:51 -0500

    new tweet straight from home page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   has_many :replies, dependent: :destroy
   belongs_to :user
 
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
 
   validates :content, presence: true, length: { maximum: 140 }
